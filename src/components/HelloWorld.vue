f4ab16270202c9f1ad9771b708e58bc2df15fcdd
SueSea <suhh@bxlsj.com>
2021-04-02 15:24:44 +0800

    首版发布

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,158 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <van-tabs v-model="month">
+            <van-tab :title="b.month" :name="b.month" v-for="(b,index1) in monthData" :key="index1">
+                <van-row type="flex" align="center" gutter="24" v-for="(a,index) in b.resultsData" :key="index">
+                    <van-col class="col" span="4">
+                        {{a.day}}
+                    </van-col>
+                    <van-col class="col" span="10">
+                        <div v-if="index==0">{{a.results}}</div>
+                        <van-field clearable input-align="center" border type='number' v-model="a.results" placeholder="请输入今日业绩" v-else />
+                    </van-col>
+                    <van-col class="col" span="10">
+                        <div v-if="index==0">{{a.person}}</div>
+                        <van-field clearable input-align="center" border type='number' v-model="a.person" placeholder="请输入今日人数" v-else />
+                    </van-col>
+                </van-row>
+                <van-row type="flex" align="center" gutter="24">
+                    <van-col class="col" span="4">
+                        总数
+                    </van-col>
+                    <van-col class="col" span="10">
+                        <van-field clearable input-align="center" border v-model="totalResults" type='number' readonly placeholder="今日业绩总数" />
+                    </van-col>
+                    <van-col class="col" span="10">
+                        <van-field clearable input-align="center" border v-model="totalPerson" type='number' readonly placeholder="今日人数总数" />
+                    </van-col>
+                </van-row>
+            </van-tab>
+        </van-tabs>
+
+    </div>
+
 </template>
 
 <script>
+import { Col, Row, Tab, Tabs, Field } from 'vant'
+import 'vant/lib/button/style'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    props: {
+        msg: String,
+    },
+    components: {
+        [Col.name]: Col,
+        [Row.name]: Row,
+        [Tab.name]: Tab,
+        [Tabs.name]: Tabs,
+        [Field.name]: Field,
+    },
+    data() {
+        return {
+            activeName: 'second',
+            month: String(new Date().getMonth() + 1),
+            totalPerson: 0,
+            totalResults: 0,
+            monthData: [],
+        }
+    },
+    conputed: {},
+    watch: {
+        monthData: {
+            handler(newVal) {
+                let locIndex = null
+                this.monthData.map((a, index) => {
+                    if (a.month == this.month) {
+                        locIndex = index
+                    }
+                })
+                if (
+                    newVal.length != 0 &&
+                    newVal[locIndex].resultsData.length != 0
+                ) {
+                    this.calresult(newVal[locIndex].resultsData)
+                    this.calrperson(newVal[locIndex].resultsData)
+                    localStorage.setItem(
+                        'monthData',
+                        JSON.stringify(this.monthData)
+                    )
+                }
+            },
+            deep: true,
+        },
+    },
+    mounted() {
+        this.monthData = localStorage.getItem('monthData')
+            ? JSON.parse(localStorage.getItem('monthData'))
+            : []
+
+        this.initData()
+    },
+    methods: {
+        initData() {
+            let days = new Date().getDate()
+            let month = String(new Date().getMonth() + 1)
+            if (this.monthData.length == 0) {
+                let monthArr = {
+                    month,
+                    resultsData: [
+                        { day: '日', results: '业绩', person: '人数' },
+                    ],
+                }
+                for (let i = 0; i < days; i++) {
+                    monthArr.resultsData.push({
+                        day: i + 1,
+                        results: null,
+                        person: null,
+                    })
+                }
+                this.monthData.push(monthArr)
+            } else if (
+                !this.monthData.map((a) => {
+                    if (a.month == month) {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+            ) {
+                let monthArr = {
+                    month,
+                    resultsData: [
+                        { day: '日', results: '业绩', person: '人数' },
+                    ],
+                }
+                for (let i = 0; i < days; i++) {
+                    monthArr.resultsData.push({
+                        day: i + 1,
+                        results: null,
+                        person: null,
+                    })
+                }
+                this.monthData.push(monthArr)
+            }
+        },
+        calresult(arr) {
+            this.totalResults = 0
+            arr.slice(1).forEach((a) => {
+                this.totalResults += Number(a.results)
+            })
+        },
+        calrperson(arr) {
+            this.totalPerson = 0
+            arr.slice(1).forEach((a) => {
+                this.totalPerson += Number(a.person)
+            })
+        },
+    },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang='scss' scoped>
+.hello {
 }
 </style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './styles/reset.css'
+import 'lib-flexible'
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
